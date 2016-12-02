bcb6f3dd7442d231acd3bca5378c98730efa4dac
Matt Widmann <mgwidmann@gmail.com>
2016-12-01 22:11:18 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Flames.Mixfile do
   def project do
     [
       app: :flames,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: compilers(Mix.env),
