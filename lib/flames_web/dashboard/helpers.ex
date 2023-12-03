91c20502f58daf35d4b72d318b2b46f3572d489e
Matt Widmann <mgwidmann@gmail.com>
2023-12-02 20:10:03 -0500

    Show each incident

--- lib/flames_web/dashboard/helpers.ex
+++ lib/flames_web/dashboard/helpers.ex
@@ -2,6 +2,7 @@ defmodule Flames.Dashboard.Helpers do
   use Phoenix.Component
 
   attr :type, :string, default: nil
+  attr :color, :atom, default: :red
   attr :class, :string, default: nil
   attr :rest, :global, include: ~w(disabled form name value)
 
@@ -12,7 +13,8 @@ defmodule Flames.Dashboard.Helpers do
     <button
       type={@type || "button"}
       class={[
-        "inline-block px-6 py-2.5 bg-rose-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-rose-700 hover:shadow-lg focus:bg-rose-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-rose-800 active:shadow-lg transition duration-150 ease-in-out",
+        "inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out",
+        button_classes_for_color(@color),
         @class
       ]}
       {@rest}
@@ -20,6 +22,18 @@ defmodule Flames.Dashboard.Helpers do
       <%= render_slot(@inner_block) %>
     </button>
     """
+  end
+
+  def button_classes_for_color(:red) do
+    "bg-rose-600 active:bg-rose-800 focus:bg-rose-700 hover:bg-rose-700"
+  end
+
+  def button_classes_for_color(:amber) do
+    "bg-amber-600 active:bg-amber-800 focus:bg-amber-700 hover:bg-amber-700"
+  end
+
+  def button_classes_for_color(_) do
+    "bg-yellow-500 active:bg-yellow-800 focus:bg-yellow-700 hover:bg-yellow-700"
   end
 
   attr :level, :string
--- lib/flames_web/dashboard/show_error_live.ex
+++ lib/flames_web/dashboard/show_error_live.ex
@@ -11,17 +11,19 @@ defmodule Flames.Dashboard.ShowErrorLive do
   end
 
   @impl true
-  def handle_params(%{"error" => id}, url, socket) do
+  def handle_params(%{"error" => id} = params, url, socket) do
     uri = URI.parse(url)
     # Drop /123
     path = Path.dirname(uri.path)
     error = Errors.get!(id)
+    {incident, ""} = Integer.parse(params["incident"] || "-1")
 
     {
       :noreply,
       socket
       |> assign(:basepath, path)
       |> assign(:error, error)
+      |> assign(:incident, incident)
     }
   end
 
@@ -37,6 +39,24 @@ defmodule Flames.Dashboard.ShowErrorLive do
           >
             <Heroicons.arrow_left solid class="w-3 h-3 stroke-current inline" /> Back
           </.link>
+        </div>
+        <div class="flex-none">
+          <.button
+            :if={@incident >= 0}
+            color={:amber}
+            phx-click={JS.push("prev")}
+            phx-value-incident={@incident - 1}
+          >
+            <Heroicons.chevron_left solid class="w-3 h-3 stroke-current inline" />
+          </.button>
+          <.button
+            :if={@incident < length(@error.incidents) - 1}
+            phx-click={JS.push("next")}
+            color={:amber}
+            phx-value-incident={@incident + 1}
+          >
+            <Heroicons.chevron_right solid class="w-3 h-3 stroke-current inline" />
+          </.button>
         </div>
         <div class="flex-none">
           <.button phx-click={JS.push("resolve")} phx-value-id={@error.id}>Resolve</.button>
@@ -75,8 +95,33 @@ defmodule Flames.Dashboard.ShowErrorLive do
           <div class="col-span-3 md:col-span-7">
             <%= @error.file %>:<%= @error.line %>
           </div>
+          <div>
+            Count:
+          </div>
+          <div class="col-span-3 md:col-span-7">
+            <%= if(@incident < 0, do: "Original", else: @incident + 1) %> out of <%= length(
+              @error.incidents
+            ) %>
+          </div>
+          <div>
+            Occurred at:
+          </div>
+          <div class="col-span-3 md:col-span-7">
+            <%= if @incident < 0 do %>
+              <%= display_timestamp(@error.timestamp) %>
+            <% else %>
+              <%= Enum.at(@error.incidents || [], @incident) &&
+                display_timestamp(
+                  (@error.incidents
+                   |> Enum.reverse()
+                   |> Enum.at(@incident)).timestamp
+                ) %>
+            <% end %>
+          </div>
         </div>
-        <pre class="whitespace-pre-wrap mt-10"><%= @error.message |> display_message() %></pre>
+        <pre class="whitespace-pre-wrap mt-10">
+          <%= pick_message(@error, @incident) |> display_message() %>
+        </pre>
       </div>
     </div>
     """
@@ -87,6 +132,29 @@ defmodule Flames.Dashboard.ShowErrorLive do
     Errors.resolve(id)
 
     {:noreply, push_navigate(socket, to: socket.assigns.basepath)}
+  end
+
+  def handle_event(prev_or_next, %{"incident" => indcident_index_string}, socket)
+      when prev_or_next in ["prev", "next"] do
+    {incident_index, ""} = Integer.parse(indcident_index_string)
+
+    {
+      :noreply,
+      socket
+      |> assign(:incident, incident_index)
+      |> push_patch(
+        to: "#{socket.assigns.basepath}/#{socket.assigns.error.id}?incident=#{incident_index}"
+      )
+    }
+  end
+
+  def pick_message(error, 0) do
+    error.message
+  end
+
+  def pick_message(error, incident_index) do
+    incident = Enum.at(error.incidents || [], incident_index)
+    (incident || error).message
   end
 
   def display_message(message) do
